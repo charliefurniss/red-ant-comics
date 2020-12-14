f1a3cd6d9ccd56966dad635e2b1469da7f2b53d1
Charlie Furniss <charliefurniss96@gmail.com>
2020-12-14 18:20:05 +0000

    Handling errors with API

--- src/components/comics/comics-list.js
+++ src/components/comics/comics-list.js
@@ -2,21 +2,29 @@ import { React, useState, useEffect } from 'react';
 
 import { Comic } from './comic';
 
+const COMICS_URL =
+  'https://gateway.marvel.com/v1/public/comics?apikey=3cb62d086d5debdeea139095cbb07fe4&ts=redant&hash=140e85a50884cef76d614f6dacada288';
+
 export const ComicsList = () => {
   const [comicsState, setComicsState] = useState({
+    error: false,
     loading: true,
     comics: null,
   });
+
   useEffect(() => {
-    setComicsState({ loading: true });
-    const url =
-      'https://gateway.marvel.com/v1/public/comics?apikey=3cb62d086d5debdeea139095cbb07fe4&ts=redant&hash=140e85a50884cef76d614f6dacada288';
-    fetch(url)
+    fetch(COMICS_URL)
       .then((res) => res.json())
-      .then((json) => {
-        setComicsState({ loading: false, comics: json.data.results });
-      });
+      .then((json) =>
+        setComicsState({ loading: false, comics: json.data.results })
+      )
+      .catch((error) => setComicsState({ error }));
   }, [setComicsState]);
+
+  if (comicsState.error)
+    return (
+      <h1>{`Sorry there's been a problem getting your comics: ${comicsState.error}`}</h1>
+    );
 
   if (comicsState.loading) return <h1>Loading...</h1>;
 
